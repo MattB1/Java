4183821b44ea0fbde20daa3b29128ddd2f3c71d2
Matt <mbubb2@myune.edu.au>
2018-08-15 16:59:13 +1000

    Adding Student Database App

---
+++ StudentDatabaseApp.java
@@ -0,0 +1,24 @@
+package studentdatabaseapp;
+
+import java.util.Scanner;
+
+public class StudentDatabaseApp {
+
+	public static void main(String[] args) {
+		// Ask user how many new students we want to add
+		System.out.println("Enter how many new user you would like to enroll: ");
+		Scanner in = new Scanner(System.in);
+		int numOfStudents = in.nextInt();
+		Student[] students = new Student[numOfStudents];
+				
+		// Create n number of new students
+		for(int n=0;n< numOfStudents;n++){
+			students[n] = new Student();
+			students[n].enroll();
+			students[n].payTuition();
+			System.out.println(students[n].toString());
+	
+		}
+	}
+
+}
